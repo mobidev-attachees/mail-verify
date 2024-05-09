344a1bfb97088e8640cddf6834049709d173d36d
Howard Koskey <koskeyhoward@gmail.com>
2024-05-09 10:15:53 +0300

    added a function to email validation

--- app/Http/Controllers/ValidationController.php
+++ app/Http/Controllers/ValidationController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class ValidationController extends Controller
 {
     //
+    //Todo create a method called validateEmail, it uses Request and accepts email address only
+    // create a results view
+    //create a route
+    public function validateEmail(Request $request){
+
+        //add logic to validate that the input provided is an email
+        // check for the other params e.g format, domain etc
+        // ultimately  store/update based on your finding
+        //return to a results view
+    }
 }
